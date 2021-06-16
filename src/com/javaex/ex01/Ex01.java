9f9657da3f7019586d8de4e690ae445a0933b607
wonho <wonho951@gmail.com>
2021-06-16 14:45:37 +0900

    2021.06.16 수업내용 I/O Stream

---
+++ src/com/javaex/ex01/Ex01.java
@@ -0,0 +1,36 @@
+package com.javaex.ex01;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Ex01 {
+
+	public static void main(String[] args) throws IOException{	//파일 던져줌
+		
+		
+		InputStream in = new FileInputStream("C:\\javaStudy\\file\\img.jpg");	//try~catch문 사용해야함.
+		
+		int data = in.read();	// 한 모금.(4바이트)
+				
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
