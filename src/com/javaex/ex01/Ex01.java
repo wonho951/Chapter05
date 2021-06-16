99f9f4dde73033706a970be85a5a2019090a3ee4
wonho <wonho951@gmail.com>
2021-06-16 15:18:47 +0900

    2021.06.16 수업내용 I/O Stream

--- src/com/javaex/ex01/Ex01.java
+++ src/com/javaex/ex01/Ex01.java
@@ -1,8 +1,10 @@
 package com.javaex.ex01;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 public class Ex01 {
 
@@ -10,8 +12,28 @@ public class Ex01 {
 		
 		
 		InputStream in = new FileInputStream("C:\\javaStudy\\file\\img.jpg");	//try~catch문 사용해야함.
+		OutputStream out = new FileOutputStream("C:\\javaStudy\\file\\byteimg.jpg");	//저장되는곳의 위치와 이름 써줘야함.
 		
-		int data = in.read();	// 한 모금.(4바이트)
+		
+		
+		
+		int data;
+		
+		System.out.println("copy start");
+		while (true) {	//느림
+			data = in.read();	// 한 모금.(4바이트)
+			
+			if(data == -1) {
+				System.out.println("copy finish: " + data);
+				break;
+			}
+			
+			out.write(data);			
+			
+		}	
+		
+		
+		
 				
 		
 		
