01142a01b299d141d722c99b6d283529e3376166
wonho <wonho951@gmail.com>
2021-06-17 12:37:08 +0900

    2021.06.17 수업내용 연습문제(난 못해 난 벌레야)

--- src/com/javaex/ex02/Ex04.java
+++ src/com/javaex/ex02/Ex04.java
@@ -1,22 +1,23 @@
 package com.javaex.ex02;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Ex04 {
 
 	public static void main(String[] args) throws IOException{
 
+		//연습문제
+		
 		//파일 읽기		
 		InputStreamReader ir = new FileReader("C:\\javaStudy\\file\\PhoneDB.txt");
 		BufferedReader br = new BufferedReader(ir);
 		
 		String str = "";
-		
+		/*
 		while (true) {
 			str = br.readLine();
 			
@@ -24,10 +25,32 @@ public class Ex04 {
 				break;
 			}
 			System.out.println(str);
-		}
+		}*/
+		
+		
+		ArrayList<Ex05> person= new ArrayList<Ex05>();
+		
+		Ex05 leejungjae = new Ex05("이정재", "010-2222-2222", "02-4332-9090");
+		Ex05 jungwoosung = new Ex05("정우성", "010-0000-2345", "02-6552-2341");
+		Ex05 yoojaeseok = new Ex05("유재석", "010-5555-5555", "02-7552-9994");
+		Ex05 leehyoelee = new Ex05("이효리", "010-9999-7777", "02-900-9888");
+		
+		
+		person.add(leejungjae);
+		person.add(jungwoosung);
+		person.add(yoojaeseok);
+		person.add(leehyoelee);
 
+		/*
+		System.out.println("이름: " + name);
+		System.out.println("핸드폰:" + hp);
+		System.out.println("회사:" + company);
+		System.out.println("");
+		*/
+		
+		
 		br.close();
-		ir.close();
+		
 		
 		
 		
--- src/com/javaex/ex02/Ex05.java
+++ src/com/javaex/ex02/Ex05.java
@@ -4,15 +4,14 @@ public class Ex05 {
 
 	//필드
 	private String name;
-	private int hp;
-	private int company;
-	
+	private String hp;
+	private String company;
 	
 	//생성자
 	public Ex05() {
 		super();
 	}
-	public Ex05(String name, int hp, int company) {
+	public Ex05(String name, String hp, String company) {
 		super();
 		this.name = name;
 		this.hp = hp;
@@ -26,27 +25,25 @@ public class Ex05 {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public int getHp() {
+	public String getHp() {
 		return hp;
 	}
-	public void setHp(int hp) {
+	public void setHp(String hp) {
 		this.hp = hp;
 	}
-	public int getCompany() {
+	public String getCompany() {
 		return company;
 	}
-	public void setCompany(int company) {
+	public void setCompany(String company) {
 		this.company = company;
 	}
-
-	
 	
 	//메소드-일반
-	
 	@Override
 	public String toString() {
 		return "Ex05 [name=" + name + ", hp=" + hp + ", company=" + company + "]";
 	}
+
 	
 	
 	
