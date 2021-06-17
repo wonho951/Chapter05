2137d8c10bfa9361f0a86801399e9e0fbee90367
wonho <wonho951@gmail.com>
2021-06-17 13:15:05 +0900

    2021.06.17 수업내용 연습문제

--- src/com/javaex/ex03/PhoneApp.java
+++ src/com/javaex/ex03/PhoneApp.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PhoneApp {
 
@@ -13,8 +15,15 @@ public class PhoneApp {
 		
 		
 		//파일 읽는다. --> 한줄씩 --> 출력
-		Reader fr = new FileReader("C:\\javaStudy\\file\\PhoneDB.txt");
+		Reader fr = new FileReader("C:\\javaStudy\\file\\PhoneDB.txt");	//빨대준비
 		BufferedReader br = new BufferedReader(fr);
+		
+		
+		
+		//보관할 리스트 만들기.
+		List<Person> pList = new ArrayList<Person>();	//리스트 준비 
+		
+		
 		
 		
 		String line = "";
@@ -36,20 +45,39 @@ public class PhoneApp {
 			String hp =  pInfo[1];
 			String company =  pInfo[2];
 			
+			Person person = new Person(name, hp, company);	//Person이 메모리에 올라옴
+			pList.add(person);	//보관되고 있다.
 			
 			
+			
+			/*
+			//한줄씩 읽고 출력중임.
 			System.out.println("이름: " + name);	//해보고 테스트해보고 반복하는게 오류 찾기 쉬움.
 			System.out.println("핸드폰: " + hp);
 			System.out.println("회사: " + company);
 			System.out.println("");
-			
-			
-			
-			
+			*/
+		}
+		
+		/*
+		System.out.println(pList.toString());	//pList의 정보 찍어보기
+		*/
+		
+		
+		//언제든지 보관하는걸 출력 가능
+		for(int i = 0; i < pList.size(); i++) {
+			System.out.println("이름: " + pList.get(i).getName());	//해보고 테스트해보고 반복하는게 오류 찾기 쉬움.
+			System.out.println("핸드폰: " + pList.get(i).getHp());
+			System.out.println("회사: " + pList.get(i).getCompany());
+			System.out.println("");
 		}
 		
 		
 		
+		System.out.println("-------------------------");
+		//언제든지 원하는거 꺼낼수 있음.
+		
+		System.out.println("정우성 정보: " + pList.get(1).getName() + "," + pList.get(1).getHp());
 		
 		br.close();
 		
