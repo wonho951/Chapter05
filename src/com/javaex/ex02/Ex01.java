0cffd56e796fc09c65d8d02b01cd63a71c4ec83a
wonho <wonho951@gmail.com>
2021-06-17 10:43:58 +0900

    2021.06.17 수업내용 reader(문자스트림)

--- src/com/javaex/ex02/Ex01.java
+++ src/com/javaex/ex02/Ex01.java
@@ -1,10 +1,39 @@
 package com.javaex.ex02;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
 public class Ex01 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException{
 
+		
+		Writer fw = new FileWriter("C:\\\\\\\\javaStudy\\\\\\\\file\\\\\\\\song.txt");	//주스트림
+		BufferedWriter br = new BufferedWriter(fw);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//문제점이 있는 방식
+		/*
+		Writer fw = new FileWriter("C:\\\\javaStudy\\\\file\\\\song.txt");	//이미 있는 파일을 넣으면 기존에 있던 파일이 망가짐.주의요망
+		String str = "학교종이 땡땡땡 어서 모이자 선생님이 우리를 기다리신다.";
+		fw.write(str);	//이건 문제점이 많다. 텍스트 양이 많아질경우?
+		fw.close();
+		*/
+		
+		
+		
 	}
 
 }
