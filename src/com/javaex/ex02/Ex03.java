c2fdd17e54ac38877db8e69c4f4edd020028252f
wonho <wonho951@gmail.com>
2021-06-17 11:39:06 +0900

    2021.06.17 수업내용 reader(UTF-8포맷 외)

--- src/com/javaex/ex02/Ex03.java
+++ src/com/javaex/ex02/Ex03.java
@@ -1,8 +1,10 @@
 package com.javaex.ex02;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class Ex03 {
 
@@ -10,8 +12,22 @@ public class Ex03 {
 
 		
 		//UTF-8 외에 다른 포맷일때
-		InputStream in = new FileInputStream("C:\\javaStudy\\file\\MS949.txt");
-		InputStreamReder isr = new
+		InputStream in = new FileInputStream("C:\\javaStudy\\file\\MS949.txt");//제일 얇은 빨대 꽂고,
+		InputStreamReader isr = new InputStreamReader(in, "MS949");//주스트림 꽂고 어떤 놈 해석할지,
+		BufferedReader br = new BufferedReader(isr);	//보조스트림(중간빨대) 꽂음. 얘가 제일 큰 빨대
+		
+		
+		String line = "";	//공간 만듦
+		
+		while(true) {
+			line = br.readLine();	//	한 줄씩 읽어
+			if(line == null) {	//한줄씩 읽다가 null이면 끝내셈	
+				break;
+			}
+			
+			System.out.println(line);
+			
+		}
 		
 		
 		
@@ -19,23 +35,12 @@ public class Ex03 {
 		
 		
 		
-		//C:\\javaStudy\\file\\MS949.txt
 		
 		
 		
 		
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		br.close();
 		
 	}
 
