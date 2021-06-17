8d28c12cc65ec0d94641f898c2924743a1899845
wonho <wonho951@gmail.com>
2021-06-17 11:33:09 +0900

    2021.06.17 수업내용 reader(UTF-8포맷 외)

--- src/com/javaex/ex02/Ex03.java
+++ src/com/javaex/ex02/Ex03.java
@@ -1,12 +1,17 @@
 package com.javaex.ex02;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Ex03 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{	//IO는 input/output임
 
 		
 		//UTF-8 외에 다른 포맷일때
-		
+		InputStream in = new FileInputStream("C:\\javaStudy\\file\\MS949.txt");
+		InputStreamReder isr = new
 		
 		
 		
