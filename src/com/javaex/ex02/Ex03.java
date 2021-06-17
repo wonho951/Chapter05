63e226c59cc38cd93fb8b80b006fd87e870c0ca8
wonho <wonho951@gmail.com>
2021-06-17 11:14:04 +0900

    2021.06.17 수업내용 reader(UTF-8포맷 외)

---
+++ src/com/javaex/ex02/Ex03.java
@@ -0,0 +1,37 @@
+package com.javaex.ex02;
+
+public class Ex03 {
+
+	public static void main(String[] args) {
+
+		
+		//UTF-8 외에 다른 포맷일때
+		
+		
+		
+		
+		
+		
+		
+		
+		//C:\\javaStudy\\file\\MS949.txt
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
